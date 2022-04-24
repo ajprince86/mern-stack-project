6497a3fa660bb3bd8d198030f84a7c1334723522
Amari Princivil <ajthacode88@gmail.com>
2022-04-24 18:19:05 -0400

    switched images

--- client/src/components/shared/ExerciseForm.js
+++ client/src/components/shared/ExerciseForm.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import apiUrl from "../../apiConfig";
 import "../css/exerciseForm.css";
+import image from "../images/workout_3.png";
+import image2 from "../images/workout_7.png";
 
 const ExerciseForm = ({
   exercise,
@@ -27,79 +29,83 @@ const ExerciseForm = ({
   }, []);
 
   return (
-    <form className="form" onSubmit={(e) => handleSubmit(e)}>
-      {/* <div className="title">Progress not perfection</div> */}
-      <div className="subtitle">Progress not perfection</div>
-      <div className="input-container ic1">
-        <select
-          className="input"
-          required
-          value={users.name}
-          onChange={(e) => handleChangeSelect(e)}
-        >
-          <option></option>
-          {users.map((user, key) => {
-            return (
-              <option value={user.name} key={key}>
-                {user.name}
-              </option>
-            );
-          })}
-        </select>
-        <div className="cut">Username:</div>
-        <label className="placeholder"></label>
-      </div>
+    <div>
+      <img className="img-left" src={image} alt="pic" />
+      <img className="img-right" src={image2} alt="pic" />
+      <form className="form" onSubmit={(e) => handleSubmit(e)}>
+        {/* <div className="title">Progress not perfection</div> */}
+        <div className="subtitle">Progress not perfection</div>
+        <div className="input-container ic1">
+          <select
+            className="input"
+            required
+            value={users.name}
+            onChange={(e) => handleChangeSelect(e)}
+          >
+            <option></option>
+            {users.map((user, key) => {
+              return (
+                <option value={user.name} key={key}>
+                  {user.name}
+                </option>
+              );
+            })}
+          </select>
+          <div className="cut">Username:</div>
+          <label className="placeholder"></label>
+        </div>
 
-      <option></option>
-      <div className="input-container ic1">
-        <input
-          required
-          placeholder=" "
-          name="description"
-          defaultValue={exercise.description}
-          id="description"
-          className="input"
-          type="text"
-          onChange={(e) => handleChange(e)}
-        />
-        <div className="cut">Workout:</div>
-        <label className="placeholder"></label>
-      </div>
-      <div className="input-container ic2">
-        <input
-          required
-          name="duration"
-          defaultValue={exercise.duration}
-          id="duration"
-          className="input"
-          type="number"
-          placeholder=" "
-          onChange={(e) => handleChange(e)}
-        />
-        <div className="cut">Duration</div>
-        <label className="placeholder"></label>
-      </div>
-      <div className="input-container ic2">
-        <input
-          required
-          name="date"
-          defaultValue={exercise.date}
-          id="date"
-          className="input"
-          type="text"
-          placeholder=" "
-          onChange={(e) => handleChange(e)}
-        />
-        <div className="cut">Date</div>
-        <label className="placeholder"></label>
-      </div>
-      <button type="submit" className="submit">
-        Submit
-      </button>
-      <Link to={cancelPath}>
-        <button className="cancel">Cancel</button>
-      </Link>
-    </form>
+        <option></option>
+        <div className="input-container ic1">
+          <input
+            required
+            placeholder=" "
+            name="description"
+            defaultValue={exercise.description}
+            id="description"
+            className="input"
+            type="text"
+            onChange={(e) => handleChange(e)}
+          />
+          <div className="cut">Workout:</div>
+          <label className="placeholder"></label>
+        </div>
+        <div className="input-container ic2">
+          <input
+            required
+            name="duration"
+            defaultValue={exercise.duration}
+            id="duration"
+            className="input"
+            type="number"
+            placeholder=" "
+            onChange={(e) => handleChange(e)}
+          />
+          <div className="cut">Duration</div>
+          <label className="placeholder"></label>
+        </div>
+        <div className="input-container ic2">
+          <input
+            required
+            name="date"
+            defaultValue={exercise.date}
+            id="date"
+            className="input"
+            type="text"
+            placeholder=" "
+            onChange={(e) => handleChange(e)}
+          />
+          <div className="cut">Date</div>
+          <label className="placeholder"></label>
+        </div>
+        <button type="submit" className="submit">
+          Submit
+        </button>
+        <Link to={cancelPath}>
+          <button className="cancel">Cancel</button>
+        </Link>
+      </form>
+    </div>
   );
 };
 
--- client/src/components/shared/UserForm.js
+++ client/src/components/shared/UserForm.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import "../css/userForm.css";
+import image from "../images/workout_2.png";
+import image2 from "../images/workout_6.png";
 
 const UserForm = ({
   user,
@@ -9,74 +11,79 @@ const UserForm = ({
   handleChangeInches,
 }) => {
   return (
-    <form className="form" onSubmit={(e) => handleSubmit(e)}>
-      <div className="title">Welcome to the journey</div>
-      <div className="subtitle">Fill out to register!</div>
-      <div className="input-container ic1">
-        <input
-          id="username"
-          defaultValue={user.name}
-          className="input"
-          name="name"
-          type="text"
-          placeholder=" "
-          onChange={(e) => handleChange(e)}
-        />
-        <div className="cut">Username:</div>
-        <label className="placeholder"></label>
-      </div>
-      <div className="input-container ic1">
-        <input
-          required
-          name="current_height"
-          defaultValue={user.current_height}
-          id="feet"
-          className="input"
-          type="number"
-          placeholder=" "
-          maxLength="7"
-          onChange={(e) => handleChange(e)}
-        />
-        <div className="cut">Foot</div>
-        <label className="placeholder"></label>
-      </div>
-      <div className="input-container ic2">
-        <input
-          required
-          name="inches"
-          defaultValue={user.current_height}
-          id="inches"
-          className="input"
-          type="number"
-          placeholder=" "
-          max="11"
-          min="0"
-          onChange={(e) => handleChangeInches(e)}
-        />
-        <div className="cut">Inches</div>
-        <label className="placeholder"></label>
-      </div>
-      <div className="input-container ic2">
-        <input
-          required
-          name="current_weight"
-          defaultValue={user.current_weight}
-          id="weight"
-          className="input"
-          type="number"
-          placeholder=" "
-          onChange={(e) => handleChange(e)}
-        />
-        <div className="cut">Weight</div>
-        <label className="placeholder"></label>
-      </div>
-      <button type="submit" className="submit">
-        Submit
-      </button>
-      <Link to={cancelPath}>
-        <button className="cancel">Cancel</button>
-      </Link>
-    </form>
+    <div>
+      <img className="img-left" src={image} alt="pic" />
+      <img className="img-right" src={image2} alt="pic" />
+
+      <form className="form" onSubmit={(e) => handleSubmit(e)}>
+        <div className="title">Welcome to the journey</div>
+        <div className="subtitle">Fill out to register!</div>
+        <div className="input-container ic1">
+          <input
+            id="username"
+            defaultValue={user.name}
+            className="input"
+            name="name"
+            type="text"
+            placeholder=" "
+            onChange={(e) => handleChange(e)}
+          />
+          <div className="cut">Username:</div>
+          <label className="placeholder"></label>
+        </div>
+        <div className="input-container ic1">
+          <input
+            required
+            name="current_height"
+            defaultValue={user.current_height}
+            id="feet"
+            className="input"
+            type="number"
+            placeholder=" "
+            maxLength="7"
+            onChange={(e) => handleChange(e)}
+          />
+          <div className="cut">Foot</div>
+          <label className="placeholder"></label>
+        </div>
+        <div className="input-container ic2">
+          <input
+            required
+            name="inches"
+            defaultValue={user.current_height}
+            id="inches"
+            className="input"
+            type="number"
+            placeholder=" "
+            max="11"
+            min="0"
+            onChange={(e) => handleChangeInches(e)}
+          />
+          <div className="cut">Inches</div>
+          <label className="placeholder"></label>
+        </div>
+        <div className="input-container ic2">
+          <input
+            required
+            name="current_weight"
+            defaultValue={user.current_weight}
+            id="weight"
+            className="input"
+            type="number"
+            placeholder=" "
+            onChange={(e) => handleChange(e)}
+          />
+          <div className="cut">Weight</div>
+          <label className="placeholder"></label>
+        </div>
+        <button type="submit" className="submit">
+          Submit
+        </button>
+        <Link to={cancelPath}>
+          <button className="cancel">Cancel</button>
+        </Link>
+      </form>
+    </div>
   );
 };
 
